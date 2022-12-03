db53d2583e7ea0c70e01d400893e08058270438c
Muhammad Usman Naeem <76055167+usman-naeem728@users.noreply.github.com>
2022-12-02 20:30:00 -0800

    express server

--- app.mjs
+++ app.mjs
@@ -1,8 +1,12 @@
 // console.log("my first server");
-
 import express from 'express';
+import cors from 'cors'
+
 const app = express()
 const port = process.env.PORT || 3000
+
+
+app.use(cors())
 
 app.get('/', (req, res) => {
 
